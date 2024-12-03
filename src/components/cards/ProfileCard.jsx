ed4cc2947ec4820389aeecf63edac164296f9e22
prathamkchandra <prathamkappu565@gmail.com>
2024-12-04 01:08:34 +0530

    feat: comps

---
+++ src/components/cards/ProfileCard.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+export default function ProfileCard() {
+    const [size, setSize] = useState(100);
+    useEffect(()=>{
+
+    },[size])
+  return (
+    <div className={`w-[${size*2}px] h-[${size}px] rounded-xl shadow-xl hover:scale-1.5 flex items-center justify-around pointer`}  onClick={()=> setSize((prev)=>prev+100)}>
+      <div className='bg-gray-300 rounded-[50%] w-[30%] h-[90%]'>
+      </div>
+      <div className='flex flex-col gap-2 text-xl font-semibold text-black'>
+        <p>Pratham Ck</p>
+        <p>19 Year old</p>
+        <p>Puttur, karnataka</p>
+      </div>
+    </div>
+  )
+}
