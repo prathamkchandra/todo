c14ff38b6b36fa8dbf43f7d3c0945dbf17bf51f2
prathamkchandra <prathamkappu565@gmail.com>
2024-12-04 01:15:31 +0530

    feat: comps1

--- src/components/cards/ProfileCard.jsx
+++ src/components/cards/ProfileCard.jsx
@@ -7,9 +7,9 @@ export default function ProfileCard() {
     },[size])
   return (
     <div className={`w-[${size*2}px] h-[${size}px] rounded-xl shadow-xl hover:scale-1.5 flex items-center justify-around pointer`}  onClick={()=> setSize((prev)=>prev+100)}>
-      <div className='bg-gray-300 rounded-[50%] w-[30%] h-[90%]'>
+      <div className='bg-gray-300 rounded-[50%] w-[30%] h-[100%]'>
       </div>
-      <div className='flex flex-col gap-2 text-xl font-semibold text-black'>
+      <div className='flex flex-col gap-3 text-xl font-semibold text-black'>
         <p>Pratham Ck</p>
         <p>19 Year old</p>
         <p>Puttur, karnataka</p>
