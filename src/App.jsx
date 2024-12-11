1aed902d29b1cf897f9da7805c49732c2ab032cb
prathamkchandra <prathamkappu565@gmail.com>
2024-12-11 18:27:34 +0530

    changes2

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ProfileCard from './components/cards/ProfileCard'
+import Navbar from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="bg-white rounded-xl shadow-xl ">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      <ProfileCard/>
+      <Navbar/> 
+    <div className="container mx-auto my-5 rounded-x1 p-5 bg-violet-100">
+        <h1 className='text-x1 '>Your todos</h1>
+      </div> 
+
     </>
   )
 }
-
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between bg-slate-900 text-white py-2">
+      <div className="logo">
+        <span className="font-bold text-x1 mx-8 my-2">itask</span>
+      </div>
+      <ul className="flex gap-8 mx-9">
+        <li className="cursor-pointer hover:font-bold transition-all duration-50">Home</li>
+        <li className="cursor-pointer hover:font-bold transition-all duration-50">Your tasks</li>
+      </ul> 
+    </nav>
+  );
+};
+
+export default Navbar;
