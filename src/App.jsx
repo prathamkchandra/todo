be703c51226e17886d05469396c265c288b4b3e8
prathamkchandra <prathamkappu565@gmail.com>
2024-12-21 11:27:12 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   return (
     <>
       <Navbar/> 
-    <div className="container mx-auto my-5 rounded-x1 p-5 bg-violet-100">
-        <h1 className='text-x1 '>Your todos</h1>
+    <div className="container mx-auto my-5 rounded-x1 p-5 w-auto bg-violet-100">
+      <div className="">
+      <h1 className='text-x1 font-bold'>Your todos</h1>
+      </div>
       </div> 
-
     </>
   )
 }
