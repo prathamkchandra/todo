4a76abbe10854328b9b9eddb21163a3c124cdcdf
prathamkchandra <prathamkappu565@gmail.com>
2024-12-21 17:44:05 +0530

    update Nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,27 @@ function App() {
   return (
     <>
       <Navbar/> 
-    <div className="container mx-auto my-5 rounded-x1 p-5 w-auto bg-violet-100">
-      <div className="">
-      <h1 className='text-x1 font-bold'>Your todos</h1>
+    <div className="container mx-auto my-5 rounded-x1 p-5 w-auto bg-violet-100 min-h-[80vh]">
+      <div className="addTodo my-5">
+        <h2 className='text-lg font-bold'>Add a Todo</h2>
+        <input type="text" className='w-2-1/2'></input>
+        <button className='bg-violet-800 hover :bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md-mx-6'>Add</button>
+        </div>
+      <h2 className='text-lg font-bold'>Your todos</h2>
+      <div className="todos">
+        < div className="todoflex">
+          <div className="text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            <div className="buttons">
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md-1'>Edit</button>
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md-2 mx-5'>Delete</button>
+              </div>
+              </div>
+              <div>
+            </div>
       </div>
-      </div> 
+</div>
+          </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-slate-100 text-white py-2">
+    <nav className="flex justify-between bg-slate-800 text-white py-2">
       <div className="logo">
-        <span className="font-bold text-x1 mx-8 my-2">liss of task</span>
+        <span className="font-bold text-x1 mx-8 my-2">list of task</span>
       </div>
       <ul className="flex gap-8 mx-9">
         <li className="cursor-pointer hover:font-bold transition-all duration-50">Home</li>
