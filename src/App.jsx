1fe3ca5b39ef5400c415237f93f2cdd66cc13293
prathamkchandra <prathamkappu565@gmail.com>
2024-12-27 12:57:01 +0530

    final change

--- src/App.jsx
+++ src/App.jsx
@@ -69,20 +69,24 @@ saveToLS()
   return (
     <>
       <Navbar/> 
-    <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[70vh] w-1/2">
-    <h1 className='font-bold text-center text-xl'>Manage your todos at one place</h1>
+    <div className="container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[70vh] md:w-[55%]">
+    <h1 className='font-bold text-center text-3xl'>Manage your todos at one place</h1>
       <div className="addTodo my-5 flex flex-col gap-4">
-        <h2 className='text-lg font-bold'>Add Todo</h2>
+        <h2 className='text-2xl font-bold'>Add Todo</h2>
+        <div className="flex">
         <input onChange={handlechange} value={todo} type="text" className='w-full rounded-full px-5 py-1'/>
-        <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-800  hover:bg-violet-950  disabled:bg-violet-400 p-2 py-1 text-sm font-bold text-white rounded-md'>Save</button>
+        <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-800  hover:bg-violet-950 mx-2 rounded-full disabled:bg-violet-400 p-4 py-2 text-sm font-bold text-white'>Save</button>
         </div>
-        <input  className='my-4' onChange={toggleFinished} type="checkbox" checked={showFinished}/>Show finished
+        </div>
+        <input  className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished}/>
+        <label className='mx-2' htmlFor="show">Show Finished</label>
+        <div className="h-[1px] bg-black opacity-15  w-[90%] mx-auto my-2"></div>
       <h2 className='text-lg font-bold'>Your todos</h2>
       <div className="todos">
         {todos.length == 0 && <div className='m-5'>No Todos yet!</div>}
         {todos.map(item=>{
 
-      return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex w-1/2 my-3 justify-between">
+      return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex my-3 justify-between">
         <div className='flex gap-5'>
         <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={todo.isCompleted} id=""/>
         <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-slate-800 text-white py-2">
+    <nav className="flex justify-around bg-slate-800 text-white py-2">
       <div className="logo">
         <span className="font-bold text-x1 mx-8 my-2">list of task</span>
       </div>
       <ul className="flex gap-8 mx-9">
         <li className="cursor-pointer hover:font-bold transition-all duration-50">Home</li>
-        <li className="cursor-pointer hover:font-bold transition-all duration-50">Your tasks</li>
   
       </ul> 
     </nav>
