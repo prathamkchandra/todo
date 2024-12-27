728cf120b52cae9a4913512e300686382844631f
prathamkchandra <prathamkappu565@gmail.com>
2024-12-27 23:48:32 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,9 @@ const Navbar = () => {
         <span className="font-bold text-x1 mx-8 my-2">list of task</span>
       </div>
       <ul className="flex gap-8 mx-9">
+
+        
         <li className="cursor-pointer hover:font-bold transition-all duration-50">Home</li>
-  
       </ul> 
     </nav>
   );
